117f4f85ef42acda4fc5a97029f3de8f936c48fb
guilherme.silva <guilherme.silva@EFL316.wds.local>
2019-09-10 10:35:51 -0300

    CRUD Livros

--- scel/src/main/java/com/fatec/scel/controller/LivroController.java
+++ scel/src/main/java/com/fatec/scel/controller/LivroController.java
@@ -1,21 +1,37 @@
 package com.fatec.scel.controller;
 
-import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import com.fatec.scel.model.Livro;
+import com.fatec.scel.model.LivroRepository;
 
-@Controller
-@RequestMapping("/livros")
+@RestController
+@RequestMapping(path = "/livros")
 public class LivroController {
-	@GetMapping("/home") // Define a url que quando for requisitada chamara o metodo
-	public String home() {
-// Retorna a view que deve ser chamada, neste exemplo a pagina home (html)
-		return "home";
+//insert into livro values ('1', 'Pressman','aaaa', 'engenharia')
+	@Autowired
+	private LivroRepository repository;
+
+	@GetMapping("/consulta")
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("consultarLivro");
+		modelAndView.addObject("livros", repository.findAll());
+		return modelAndView;
 	}
 
+	/**
+	 * quando o usuario digita localhost:8080/api/add
+	 *
+	 * @param livro
+	 * @return o html /CadastraLivro
+	 */
 	@GetMapping("/cadastrar")
 	public ModelAndView cadastraLivro(Livro livro) {
 		ModelAndView mv = new ModelAndView("cadastrarLivro");
@@ -23,11 +39,58 @@ public class LivroController {
 		return mv;
 	}
 
-	@PostMapping("/salvar")
-	public ModelAndView adicionar(Livro livro) {
-		System.out.println("ISBN digitado na interface ===>" + livro.getIsbn());
+	@GetMapping("/edit/{isbn}") // diz ao metodo que ira responder a uma requisicao do tipo get
+	public ModelAndView mostraFormAdd(@PathVariable("isbn") String isbn) {
+		ModelAndView modelAndView = new ModelAndView("atualizaLivro");
+		modelAndView.addObject("livro", repository.findByIsbn(isbn)); // o repositorio e injetado no controller
+		return modelAndView; // addObject adiciona objetos para view
+	}
+
+	@GetMapping("/delete/{id}")
+	public ModelAndView delete(@PathVariable("id") Long id) {
+		repository.deleteById(id);
 		ModelAndView modelAndView = new ModelAndView("consultarLivro");
-		modelAndView.addObject("message", livro.getIsbn());
+		modelAndView.addObject("livros", repository.findAll());
+		return modelAndView;
+	}
+
+	@PostMapping("/save")
+	public ModelAndView save(@Valid Livro livro, BindingResult result) {
+		ModelAndView modelAndView = new ModelAndView("consultarLivro");
+		if (result.hasErrors()) {
+			return new ModelAndView("cadastrarLivro");
+		}
+		try {
+			Livro jaExiste = null;
+			jaExiste = repository.findByIsbn(livro.getIsbn());
+			if (jaExiste == null) {
+				repository.save(livro);
+				modelAndView = new ModelAndView("consultarLivro");
+				modelAndView.addObject("livros", repository.findAll());
+				return modelAndView;
+			} else {
+				return new ModelAndView("cadastrarLivro");
+			}
+		} catch (Exception e) {
+			System.out.println("erro ===> " + e.getMessage());
+			return modelAndView; // captura o erro mas nao informa o motivo.
+		}
+	}
+
+	@PostMapping("/update/{id}")
+	public ModelAndView atualiza(@PathVariable("id") Long id, @Valid Livro livro, BindingResult result) {
+
+		if (result.hasErrors()) {
+			livro.setId(id);
+			return new ModelAndView("atualizaLivro");
+		}
+		Livro umLivro = repository.findById(id).get();
+		umLivro.setAutor(livro.getAutor());
+		umLivro.setIsbn(livro.getIsbn());
+		umLivro.setTitulo(livro.getTitulo());
+		repository.save(umLivro);
+		ModelAndView modelAndView = new ModelAndView("consultarLivro");
+		modelAndView.addObject("livros", repository.findAll());
 		return modelAndView;
 	}
 }
